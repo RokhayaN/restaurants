513e6d150c2c80a7de09f0be004eabfd0feaad5c
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-01 15:35:22 +0000

    cli.rb

---
+++ lib/restaurantsL/cli.rb
@@ -0,0 +1,11 @@
+require 'nokogiri'
+require 'open-uri'
+
+def execute 
+  puts "Are you a foodie like me ?"
+  puts " Theses is a list of the best ethiopian restaurants in Washington DC "
+  puts " "
+  puts "---------------------------------------"
+  puts " "
+end 
+  
