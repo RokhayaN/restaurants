9a82ecfc60c686c41d4f18baada9938ad382afe9
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-03 17:17:11 +0000

    restaurants

--- lib/restaurantsL/cli.rb
+++ lib/restaurantsL/cli.rb
@@ -1,7 +1,5 @@
 class Cli 
   
-end
-
 def execute 
   puts "Are you a foodie like me ?"
   puts "Theses is a list of the best ethiopian restaurants in DC. "
@@ -9,16 +7,11 @@ def execute
   puts "-----------------------------------------"
   puts " "
   
-  html = open("https://www.tripadvisor.com/Restaurants-g28970-c10785-Washington_DC_District_of_Columbia.html")
-  restaurants = [ ]
-  doc = Nokogiri::HTML(html)
-  doc.css("div.ui_column.is-narrow.title_wrap").each do |r|
-    restaurantName = r.css("a.restaurants-list-ListCell__restaurantName--2aSdo").text.strip
-    restaurants << restaurantName
   end 
-  restaurants.each.with_index(1) do |restaurantName,index|
+  
+ def show_restaurants
+   Restaurant.all.each.with_index(1) do |restaurantName,index|
   puts "#{restaurantName}"
-  #binding.pry
- end 
+end 
 end 
   
