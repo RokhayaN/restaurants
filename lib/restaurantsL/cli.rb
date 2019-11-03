3dc71f4a86f264d846e1554b2758b1c30638d68b
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-03 03:52:39 +0000

    restaurants

--- lib/restaurantsL/cli.rb
+++ lib/restaurantsL/cli.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+
 def execute 
   puts "Are you a foodie like me ?"
   puts "Theses is a list of the best ethiopian restaurants in DC. "
@@ -11,7 +12,8 @@ def execute
   
   html = open("https://www.tripadvisor.com/Restaurants-g28970-c10785-Washington_DC_District_of_Columbia.html")
   doc = Nokogiri::HTML(html)
-  doc.css("div.ui_column.is-narrow.title_wrap")
-  binding.pry
+  doc.css("div.ui_column.is-narrow.title_wrap").each do |r|
+  #binding.pry
+  end 
 end 
   
