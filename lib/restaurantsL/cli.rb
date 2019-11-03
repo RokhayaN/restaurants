35031bc2b66fc9458286ce5e7ece25b8b3ff30a4
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-03 04:19:22 +0000

    restaurants

--- lib/restaurantsL/cli.rb
+++ lib/restaurantsL/cli.rb
@@ -13,6 +13,7 @@ def execute
   html = open("https://www.tripadvisor.com/Restaurants-g28970-c10785-Washington_DC_District_of_Columbia.html")
   doc = Nokogiri::HTML(html)
   doc.css("div.ui_column.is-narrow.title_wrap").each do |r|
+    name = r.css("a.restaurants-list-ListCell__restaurantName--2aSdo").text
   binding.pry
   end 
 end 
