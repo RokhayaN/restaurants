b5d0111046cfc9ff8db58a8d21a1c283f088331c
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-04 17:07:58 +0000

    restaurants

--- lib/restaurantsL/cli.rb
+++ lib/restaurantsL/cli.rb
@@ -2,28 +2,21 @@ class Cli
   
   def execute 
     puts "Are you a foodie like me ?"
-    puts "Theses is a list of the best ethiopian restaurants in DC. "
+    puts "Theses are the best ethiopian restaurants in DC. "
     puts " "
     puts "-----------------------------------------"
     puts " "
     Scraper.scrape_restaurants
+    show_restaurants
     binding.pry
     #loop that asks user what to do and does that until "exit"
-  
-    #list_layout
-    #show_restaurants
-  
   end 
   
-  
-  #def lay_out
-  #end 
-  
-  #def show_restaurants
-   # Restaurant.all.each.with_index(1) do |restaurant,index|
-      #puts "#{index},#{restaurant.name}"
-    #end
-  #end 
+  def show_restaurants
+    Restaurant.all.each.with_index(1) do |restaurant,index|
+      puts "#{index}.#{restaurant.name}"
+    end
+  end 
 end 
 
 
