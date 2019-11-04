4fdb98ec8c34d1ace9ea8e051486d444162f84cc
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-04 21:14:48 +0000

    restaurants

--- lib/restaurantsL/scraper.rb
+++ lib/restaurantsL/scraper.rb
@@ -2,11 +2,12 @@ class Scraper
   
   def self.scrape_restaurants
     
-    html = open("https://www.tripadvisor.com/Restaurants-g28970-c10785-Washington_DC_District_of_Columbia.html")
+    html = open("https://guide.michelin.com/us/en/washington/washington-dc/article/dining-out/ethiopian-food-restaurants-washington-dc#")
     doc = Nokogiri::HTML(html)
-    doc.css("div.ui_column.is-narrow.title_wrap").each do |r|
+    binding.pry
+    doc.css(".detail-page__content h2 a").each do |r|
       restaurant = Restaurant.new
-      restaurant.name = r.css("a.restaurants-list-ListCell__restaurantName--2aSdo").text.strip.split(". ")[1]
+      restaurant.name = r.css(".detail-page__content h2 a").text.strip.split(". ")[1]
       restaurant.url = r.css("a").attribute("href").value
     end
   end  
