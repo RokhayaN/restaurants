ab3fe426a268756787410401b66589ef331e4d87
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-04 15:43:08 +0000

    restaurants

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,6 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-require_relative "../lib/restaurantsL/cli.rb"
-require_relative "../lib/restaurantsL/scraper.rb"
-require_relative "../lib/restaurantsL/restaurant.rb"
+require_relative "./lib/restaurantsL/cli.rb"
+require_relative "./lib/restaurantsL/scraper.rb"
+require_relative "./lib/restaurantsL/restaurant.rb"
